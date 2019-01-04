376bfbb003f02d30502247a549d1c9623def8895
ntalabot <nicolas.talabot@gmail.com>
2019-01-04 10:22:37 +0100

    Started testing cv-annotations

--- deep_learning/run_gridsearch.py
+++ deep_learning/run_gridsearch.py
@@ -25,7 +25,7 @@ def main():
     # Arguments for run_train
     args = Arguments(
             batch_size = 32,
-            data_dir = "/data/talabot/dataset/",
+            data_dir = "/data/talabot/dataset_cv-annotated/",
             epochs = n_epochs,
             eval_test = False,
             input_channels = "R",
@@ -43,7 +43,31 @@ def main():
             verbose = False
     )
     model = None
+    args.synthetic_data = True
     
+    print("Input channels: R")
+    for synth_ratio in synth_ratios:
+        args.synthetic_ratio = synth_ratio
+        print("synth_ratio={: <4}".format(synth_ratio), end="", flush=True)
+                    
+        try:
+            model = CustomUNet(len(args.input_channels), 
+                               u_depth = 4,
+                               out1_channels = 16, 
+                               batchnorm = True)
+            history = run_train.main(args, model=model)
+            best_epoch = np.argmax(history["val_dice"])
+            print(" | loss={:.6f} - lossC{:.1f}={:.6f} - dice={:.6f} - diC{:.1f}={:.6f}".format(
+                    history["val_loss"][best_epoch], 
+                    args.scale_crop, history["val_lossC%.1f" % args.scale_crop][best_epoch],
+                    history["val_dice"][best_epoch],
+                    args.scale_crop, history["val_diC%.1f" % args.scale_crop][best_epoch]))
+            
+        except RuntimeError as err: # CUDA out of memory
+            print(" | RuntimeError ({})".format(err))
+            
+    args.input_channels = "RG"
+    print("\nInput channels: RG")
     for synth_ratio in synth_ratios:
         args.synthetic_ratio = synth_ratio
         print("synth_ratio={: <4}".format(synth_ratio), end="", flush=True)
@@ -65,7 +89,7 @@ def main():
             print(" | RuntimeError ({})".format(err))
                             
     # If an error occured, this is not printed
-    # TODO: is there a way to force this printing ? try-except does not work with KeyboardInterrupt
+    # TODO: is there a way to force this to print ? try-except does not work with KeyboardInterrupt
     duration = time.time() - start_time
     duration_msg = "{:.0f}h {:02.0f}min {:02.0f}s".format(duration // 3600, (duration // 60) % 60, duration % 60)
     print("\nEnding on %s" % time.ctime())
